70edd7842424cb803ef8c90a9263ab0e7f63ab8b
Gordon Choi <thefirate@gmail.com>
2022-03-04 01:39:53 +0900

    Apply hard-coded value for test

--- GoodWeather/Controllers/WeatherListTableViewController.swift
+++ GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -19,3 +19,24 @@ extension WeatherListTableViewController {
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
 }
+
+extension WeatherListTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath) as! WeatherCell
+        cell.cityNameLabel?.text = "Houston"
+        cell.temperatureLabel?.text = "70°"
+        return cell
+    }
+}
