274095111138053aa0da057624591754a717fc50
Gordon Choi <thefirate@gmail.com>
2022-04-03 00:54:18 +0900

    Set up settings screen

---
+++ GoodWeather/Controllers/SettingsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.swift
+//  GoodWeather
+//
+//  Created by Gordon Choi on 2022/04/03.
+//
+
+import Foundation
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+    
+}
---
+++ GoodWeather/ViewModels/SettingsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewModel.swift
+//  GoodWeather
+//
+//  Created by Gordon Choi on 2022/04/03.
+//
+
+import Foundation
