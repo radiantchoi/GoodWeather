bdc72b2a1524b9c08390cb425c3af6d830c8e13f
Gordon Choi <thefirate@gmail.com>
2022-04-03 02:12:36 +0900

    Make unit selectable and deselectable

--- GoodWeather/Controllers/SettingsTableViewController.swift
+++ GoodWeather/Controllers/SettingsTableViewController.swift
@@ -32,6 +32,29 @@ extension SettingsTableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath)
         cell.textLabel?.text = settingsItem.displayName
         
+        if settingsItem == settingsViewModel.selectedUnit {
+            cell.accessoryType = .checkmark
+        }
+        
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // uncheck all cells
+        tableView.visibleCells.forEach { cell in
+            cell.accessoryType = .none
+        }
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .checkmark
+            let unit = Unit.allCases[indexPath.row]
+            settingsViewModel.selectedUnit = unit
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .none
+        }
     }
 }
